6c5c41fcaf7b2970a618b36f9d963dac6268e7a2
FA1C0Nl-lAWK <falcor.peterson@gmail.com>
2023-02-14 11:20:29 -0500

    created first draft for post class and methods

--- hcproject/homeCooked/models.py
+++ hcproject/homeCooked/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(producerID, recipeID):
+    postid = 0;
+    producerID = producerID;
+    recipeID = recipeID;
+    available = True;
+    consumerID = 0;
--- hcproject/homeCooked/views.py
+++ hcproject/homeCooked/views.py
@@ -1,5 +1,36 @@
 from django.http import HttpResponse
+import datetime
 
 
 def index(request):
     return HttpResponse("Hello, world!")
+
+def getPostsByUser(userid):
+    # SELECT postid FROM Posts WHERE producerid=userid;
+    return [];
+
+def createPost(postid, producerid, recipeid):
+    timeStamp = datetime.datetime.now();
+    # verify valid ids (producer and recipe exist and post doesn't)
+
+    # INSERT INTO Posts (postid, producerid, recipeid, available, created)
+    # VALUES (postid, producerid, recipeid, True, timeStamp);
+
+    """
+    CREATE TABLE Posts(
+        postid Number(8),
+        producerid Number(16),
+        recipeid Number(8),
+        consumerid Number(8),
+        available Bool(1)
+        created date,
+        compleated date
+    );
+    """
+def getTransactionsByUser(userid):
+    # SELECT postid FROM posts WHERE consumerid=userid OR producerid=userid;
+    return
+
+def completeTransaction(postid, consumerid):
+    timeStamp = datetime.datetime.now();
+    # UPDATE Posts SET consumerid=userid, available=False, compleated=timeStamp WHERE postid=postid;
