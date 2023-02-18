86db40b6dffd31dcfe7c39bf9367064e60915169
Akhil Thata <akhil.thata@gmail.com>
2023-02-18 15:45:39 -0500

    Added DashBoard

--- hcproject-frontend/src/App.js
+++ hcproject-frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Settings from "./pages/Settings";
 import Login from "./pages/Login/Login"
 import Signup from "./pages/Signup/Signup";
+import Dashboard from "./pages/Dashboard/Dashboard"
 import "./App.css";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/settings" element={<Settings />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard/>} />
       </Routes>
     </Router>
   );
---
+++ hcproject-frontend/src/pages/Dashboard/Dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { useState } from 'react';
+const Dashboard = () => {
+  const [userMode, setUserMode] = useState("consumer");
+  return (
+    <div>Dashboard</div>
+  )
+}
+
+export default Dashboard
