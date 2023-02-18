9af83d01575dd9cb23ee4db659081685f52c612a
Sebastian <61755980+SebastianD101@users.noreply.github.com>
2023-02-17 20:21:25 -0500

    finished setting up sql server and admin page

--- hcproject/homeCooked/admin.py
+++ hcproject/homeCooked/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Event)
+admin.site.register(Recipe)
+admin.site.register(Review)
+admin.site.register(Post)
+admin.site.register(Message)
--- hcproject/homeCooked/models.py
+++ hcproject/homeCooked/models.py
@@ -1,3 +1,75 @@
 from django.db import models
 
-# Create your models here.
+class User (models.Model):
+    user_id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    biography = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.user_name
+
+class Event (models.Model):
+    event_id = models.AutoField(primary_key=True)
+    event_name = models.CharField(max_length=200)
+    event_desc = models.CharField(max_length=200)
+    event_date = models.DateField()
+    event_time = models.TimeField()
+    event_location = models.CharField(max_length=200)
+    event_capacity = models.IntegerField()
+    event_host = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.event_name
+
+class Recipe (models.Model):
+    recipe_id = models.AutoField(primary_key=True)
+    recipe_desc = models.CharField(max_length=200)
+    recipe_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    recipe_name = models.CharField(max_length=200)
+    recipe_ingredients = models.CharField(max_length=200)
+    recipe_img = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.recipe_name
+
+class Review (models.Model):
+    review_id = models.AutoField(primary_key=True)
+    review_desc = models.CharField(max_length=200)
+    review_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    review_recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    review_rating = models.IntegerField()
+
+    def __str__(self):
+        return self.review_desc
+
+class Post (models.Model):
+    post_id = models.AutoField(primary_key=True)
+    post_desc = models.CharField(max_length=200)
+    post_producer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='producer')
+    post_consumer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='consumer')
+    post_review = models.ForeignKey(Review, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.post_desc
+
+class Message (models.Model):
+    message_id = models.AutoField(primary_key=True)
+    message = models.CharField(max_length=200)
+    message_sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender')
+    message_recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipient')
+
+    def __str__(self):
+        return self.message_desc
+
+class DiscussionBoard (models.Model):
+    discussion_id = models.AutoField(primary_key=True)
+    discussion_desc = models.CharField(max_length=200)
+    discussion_sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    discussion_event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    discussion_data = models.DateField()
+
+    def __str__(self):
+        return self.discussion_name
