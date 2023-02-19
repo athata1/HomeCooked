3df8adf9dfebf52e9be40c3061f66ed8d518e534
Velagapudi, Yash <yvelagap@purdue.edu>
2023-02-18 23:39:15 -0500

    Added about, save, delete account

--- hcproject-frontend/src/pages/Settings/Settings.js
+++ hcproject-frontend/src/pages/Settings/Settings.js
@@ -5,7 +5,9 @@ import { CgProfile } from "react-icons/cg";
 const Settings = () => {
   return (
     <div className="px-5">
-      <h1 className="settings-title">Account</h1>
+      <div>
+        <h1 className="settings-title">Account</h1>
+      </div>
       <div className="settings-line"></div>
       <form className="settings-form">
         <div className="row">
@@ -41,10 +43,41 @@ const Settings = () => {
             />
           </div>
         </div>
-        <div className="mt-3">
-          <label className="settings-label">Photo </label>
-          <br />
-          <CgProfile size={80} />
+        <div className="row mt-3">
+          <div className="col">
+            <h3 className="settings-label">Photo</h3>
+          </div>
+        </div>
+        <div className="row mt-2">
+          <div className="col">
+            <button className="btn">
+              <CgProfile size={80} />
+            </button>
+            <button className="mx-5 btn settings-button">Change</button>
+            <button className="btn btn-danger settings-button-remove">
+              Remove
+            </button>
+          </div>
+        </div>
+        <div className="row mt-5">
+          <div className="col">
+            <h3 className="settings-label">About</h3>
+          </div>
+        </div>
+        <div className="row mb-5">
+          <div className="col">
+            <textarea className="settings-textarea"></textarea>
+          </div>
+        </div>
+        <div className="col mb-3 settings-save-div d-flex justify-content-center">
+          <button className="mx-5 btn settings-button settings-save-button">
+            Save
+          </button>
+        </div>
+        <div className="col mb-5 settings-save-div d-flex justify-content-center">
+          <button className="btn btn-danger settings-button-remove settings-delete-account-button">
+            Delete Account
+          </button>
         </div>
       </form>
     </div>
