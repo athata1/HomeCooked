9556b1a9c729becfd8e9bc65e8b59d2016e28f59
Akhil Thata <akhil.thata@gmail.com>
2023-02-18 19:58:08 -0500

    Added padding to login and signup

--- hcproject-frontend/src/pages/Login/Login.js
+++ hcproject-frontend/src/pages/Login/Login.js
@@ -5,20 +5,22 @@ import "./Login.css"
 
 export default function Login() {
   return (
-    <div className="login">
+    <>
       <div className="login-background"></div>
-      <div className="login-loginform">
-        <div className="login-title">HomeCooked</div>
-        <input className="login-input" type="email" placeholder='Enter Email Address'></input>
-        <input className="login-input" type="password" placeholder='Enter Password'></input>
-        <button className="login-button" disabled={false}>Login</button>
-        <div className='signup-forgot-links'>
-        <Link to="/signup" style={{ textDecoration: 'none' }}>
-          <div className="signup-link">Sign Up</div>
-        </Link>
-          <div className='forgot-password-link'>Forgot Password</div>
+      <div className="login">
+        <div className="login-loginform">
+          <div className="login-title">HomeCooked</div>
+          <input className="login-input" type="email" placeholder='Enter Email Address'></input>
+          <input className="login-input" type="password" placeholder='Enter Password'></input>
+          <button className="login-button" disabled={false}>Login</button>
+          <div className='signup-forgot-links'>
+          <Link to="/signup" style={{ textDecoration: 'none' }}>
+            <div className="signup-link">Sign Up</div>
+          </Link>
+            <div className='forgot-password-link'>Forgot Password</div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
--- hcproject-frontend/src/pages/Signup/Signup.js
+++ hcproject-frontend/src/pages/Signup/Signup.js
@@ -5,17 +5,19 @@ import "./Signup.css"
 
 export default function Signup() {
   return (
-    <div className="signup">
+    <>
       <div className="signup-background"></div>
-      <div className="signup-signupform">
-        <div className="signup-title">HomeCooked</div>
-        <input className="signup-input" type="email" placeholder='Enter Email Address'></input>
-        <input className="signup-input" type="text" placeholder='Enter Username'></input>
-        <input className="signup-input" type="password" placeholder='Enter Password'></input>
-        <input className="signup-input" type="password" placeholder='Confirm Password'></input>
-        <button className="signup-button" disabled={false}>Sign Up</button>
-      <div className="signup-login">Already have an account: <Link to="/login" style={{ textDecoration: 'none' }}><span className="signup-login-link">Login</span></Link></div>
+      <div className="signup">
+        <div className="signup-signupform">
+          <div className="signup-title">HomeCooked</div>
+          <input className="signup-input" type="email" placeholder='Enter Email Address'></input>
+          <input className="signup-input" type="text" placeholder='Enter Username'></input>
+          <input className="signup-input" type="password" placeholder='Enter Password'></input>
+          <input className="signup-input" type="password" placeholder='Confirm Password'></input>
+          <button className="signup-button" disabled={false}>Sign Up</button>
+        <div className="signup-login">Already have an account: <Link to="/login" style={{ textDecoration: 'none' }}><span className="signup-login-link">Login</span></Link></div>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
