4851ae183500a74bd751f0cd4b672028b670150a
Akhil Thata <akhil.thata@gmail.com>
2023-02-18 19:40:58 -0500

    Created login page

--- hcproject-frontend/src/pages/Login/Login.js
+++ hcproject-frontend/src/pages/Login/Login.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import {Link} from "react-router-dom"
+import "./Login.css"
+
 
 export default function Login() {
   return (
     <div className="login">
-      Login
+      <div className="login-background"></div>
+      <div className="login-signupform">
+        <div className="login-title">HomeCooked</div>
+        <input className="login-input" type="email" placeholder='Enter Email Address'></input>
+        <input className="login-input" type="password" placeholder='Enter Password'></input>
+        <button className="login-button" disabled={false}>Login</button>
+        <div className='signup-forgot-links'>
+        <Link to="/signup" style={{ textDecoration: 'none' }}>
+          <div className="signup-link">Sign Up</div>
+        </Link>
+          <div className='forgot-password-link'>Forgot Password</div>
+        </div>
+      </div>
     </div>
   )
 }
--- hcproject-frontend/src/pages/Signup/Signup.js
+++ hcproject-frontend/src/pages/Signup/Signup.js
@@ -14,7 +14,7 @@ export default function Signup() {
         <input className="signup-input" type="password" placeholder='Enter Password'></input>
         <input className="signup-input" type="password" placeholder='Confirm Password'></input>
         <button className="signup-button" disabled={false}>Sign Up</button>
-      <div className="signup-login">Already have an account: <Link to="/login"><span className="signup-login-link">Login</span></Link></div>
+      <div className="signup-login">Already have an account: <Link to="/login" style={{ textDecoration: 'none' }}><span className="signup-login-link">Login</span></Link></div>
       </div>
     </div>
   )
