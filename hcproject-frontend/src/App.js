748c95cc49abb1729317de3825458e7a869124e5
Akhil Thata <akhil.thata@gmail.com>
2023-02-21 19:09:57 -0500

    Added signup functionality

--- hcproject-frontend/src/App.js
+++ hcproject-frontend/src/App.js
@@ -4,17 +4,20 @@ import Settings from "./pages/Settings/Settings";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
 import Dashboard from "./pages/Dashboard/Dashboard"
+import { AuthProvider } from "./Firebase/AuthContext";
 import "./App.css";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/settings" element={<Settings />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/dashboard" element={<Dashboard/>} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/dashboard" element={<Dashboard/>} />
+        </Routes>
+      </AuthProvider>
     </Router>
   );
 }
