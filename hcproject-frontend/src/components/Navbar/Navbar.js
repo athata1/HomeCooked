d0d28e7b5f4ba84e58464f2a2de86db286d0ab76
Akhil Thata <akhil.thata@gmail.com>
2023-02-24 15:56:17 -0500

    Added Logo to Navbar

--- hcproject-frontend/src/components/Navbar/Navbar.js
+++ hcproject-frontend/src/components/Navbar/Navbar.js
@@ -1,10 +1,16 @@
 import React from "react";
 import './Navbar.css'
 import ProfileDropdown from '../ProfileDropdown/ProfileDropdown'
+import {BsHouseDoor} from 'react-icons/bs'
+import {Link} from 'react-router-dom'
 
 function Navbar({data}) {
   return <div className="navbar">
-    <div className="navbarLeft">left</div>
+    <div className="navbarLeft">
+      <Link to="/dashboard">
+        <BsHouseDoor size={40} style={{color: "#ffffff"}}/>
+      </Link>
+    </div>
     <div className="navbarMiddle">
       <div className="searchInput">
         <input className="navbar-search" type="text" placeholder={"Find profiles, posts, events..."} /> 
@@ -12,8 +18,9 @@ function Navbar({data}) {
       </div>
       <div className="searchResult"></div>
     </div>
-    <div className="navbarRight">right</div>
+    <div className="navbarRight">
       <ProfileDropdown />
+    </div>
   </div>;
 };
 
