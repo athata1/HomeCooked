8162e96f5847a72dd6e75f5a02e2bce4b8ed4df6
Colton Scott <83851807+coltscott@users.noreply.github.com>
2023-02-27 15:56:05 -0500

    user update

--- hcproject/hcproject/settings.py
+++ hcproject/hcproject/settings.py
@@ -127,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_ALLOW_ALL = True
