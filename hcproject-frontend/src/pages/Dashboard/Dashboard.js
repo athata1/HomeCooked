d39eaaa1346a15f2586be548f8f58cf79fc174a3
Vincent Cui <vcui@purdue.edu>
2023-02-28 21:58:57 -0500

    further new post UI

--- hcproject-frontend/src/pages/Dashboard/Dashboard.js
+++ hcproject-frontend/src/pages/Dashboard/Dashboard.js
@@ -8,26 +8,116 @@ import CardGroup from 'react-bootstrap/CardGroup';
 import Navbar from '../../components/Navbar/Navbar';
 import { useAuth } from '../../Firebase/AuthContext';
 import Posts from '../../components/Posts/Posts';
+import InputTag from '../../components/InputTag/InputTag';
 
 const Dashboard = () => {
-  const { currentUser, getToken, userMode, setUserMode, loading } = useAuth();
+  const { currentUser, getToken, userMode, setUserMode } = useAuth();
   const [token, setToken] = useState();
 
+  const handleNewPost = (e) => {
+    e.preventDefault();
+  };
 
   useEffect(() => {
     getToken().then((t) => {
       setToken(t);
-      console.log(t);
     });
-    console.log("Here");
   }, []);
 
   return (
     <div className="dashboard">
       <Navbar part="Posts" mode={userMode} />
       <span>&nbsp;&nbsp;</span>
+      <div className="col mb-5 settings-save-div d-flex justify-content-center">
+        <button
+          className="btn btn-primary settings-button-remove settings-delete-account-button"
+          data-bs-toggle="modal"
+          data-bs-target="#exampleModal"
+          onClick={handleNewPost}
+        >
+          New Post
+        </button>
+        <div
+          className="modal fade"
+          id="exampleModal"
+          tabIndex="-1"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">
+                  Create a post
+                </h5>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="modal-body">
+                <input
+                  type="title"
+                  className="form-control settings-input"
+                  placeholder="Title"
+                // value={deleteAccountPassword}
+                // onChange={(e) => setDeleteAccountPassword(e.target.value)}
+                />
+                <span>&nbsp;&nbsp;</span>
+                <textarea
+                  placeholder="Text"
+                  className="form-control"
+                  id="email_body"
+                  rows="7"
+                // placeholder="Text"
+                // type="textarea"
+                // multiline={true} 
+                // style={{padding: 10}} 
+                // textAlignVertical="top"
+                // className="form-control settings-input"
+                // style={{height: "100px"}}
+
+                // value={deleteAccountPassword}
+                // onChange={(e) => setDeleteAccountPassword(e.target.value)}
+                />
+                <span>&nbsp;&nbsp;</span>
+
+                <div>
+                  <label for="formFileLg" class="form-label">Input Image</label>
+                  <input class="form-control form-control-md" id="formFileLg" type="file" />
+                </div>
+                
+
+                <div>test</div>
+
+
+              </div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                >
+                  Close
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-primary"
+                  data-bs-dismiss="modal"
+                // onClick={confirmDeleteAccount}
+                >
+                  Publish Post
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <Container>
         <Row>
+
           <Col>
             <h2>Post Feed</h2>
             <Posts />
@@ -58,7 +148,8 @@ const Dashboard = () => {
 
       </Container>
 
-      {currentUser ? token: ""}
+      {/* Dashboard
+      {currentUser ? currentUser.email : ""} */}
     </div>
 
   )
