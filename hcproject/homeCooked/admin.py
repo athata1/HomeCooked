041d3f28e60968c6750a1b9ea39170ddba06ca91
FA1C0Nl-lAWK <falcor.peterson@gmail.com>
2023-04-14 19:30:12 -0400

    Merging changes to main

--- hcproject/homeCooked/admin.py
+++ hcproject/homeCooked/admin.py
@@ -7,7 +7,7 @@ class AllergyAdmin(admin.ModelAdmin):
     search_fields = ('food_name', 'health_labels')
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('user_id', 'user_uname', 'user_fid', 'user_address', 'user_bio', 'user_city', 'user_state', 'image_text', 'user_longitude', 'user_latitude')
+    list_display = ('user_id', 'user_uname', 'user_fid', 'user_address', 'user_bio', 'user_city', 'user_state', 'image_text', 'user_longitude', 'user_latitude', 'user_link')
     list_display_links = ('user_id', 'user_fid')
     list_filter = ('user_id', 'user_uname', 'user_fid', 'user_city', 'user_state')
     search_fields = ('user_id', 'user_uname', 'user_fid', 'user_city', 'user_state')
