cdaa7309a7599eda86fbd8c6d32bf683e2340071
Yash V <yashv6655@gmail.com>
2023-04-17 14:41:30 -0400

    Co-authored-by: Colton Scott <coltscott@users.noreply.github.com> Co-authored-by: Sebastian <SebastianD101@users.noreply.github.com>

--- hcproject-frontend/src/App.js
+++ hcproject-frontend/src/App.js
@@ -18,6 +18,7 @@ import OtherProfiles from "./pages/OtherProfiles/OtherProfiles";
 import ChangePassword from "./pages/ChangePassword/ChangePassword";
 import ResetPassword from "./pages/ResetPassword/ResetPassword";
 import Chat from "./pages/Chat/Chat";
+import Notifications from "./pages/Notifications/Notifications"
 import { ReactNotifications } from 'react-notifications-component'
 import 'react-notifications-component/dist/theme.css'
 
